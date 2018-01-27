96fed0733e2d30ed043018948b1c0c9f48f9f8d5
Razzi Abuissa <razzi53@gmail.com>
2018-01-27 15:52:08 -0800

    Server sends emoji to clients

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ReconnectingWebsocket from 'reconnecting-websocket'
+
 import './App.css';
 import Messenger from './Messenger.react';
 import Scrambler from './components/Scrambler.react';
@@ -10,6 +12,38 @@ const PAGES = {
   VOTER: 'VOTER',
 };
 
+const host = 'localhost:8000'
+
+function getWsProtocol() {
+  if (window.location.protocol === 'https:') {
+    return 'wss://'
+  } else {
+    return 'ws://'
+  }
+}
+
+function sendMessage(ws, message) {
+  ws.send(JSON.stringify(message))
+}
+
+function handleMessage(message) {
+  const reader = new FileReader()
+
+  reader.onload = () => {
+    const data = JSON.parse(reader.result)
+
+    console.log(`Received ${JSON.stringify(data)}`)
+
+    if (data.type === 'receive_emoji') {
+      var updatedState = this.state
+      updatedState['emoji'] = message.emoji
+      this.setState(updatedState)
+    }
+  }
+
+  reader.readAsText(message.data)
+}
+
 class App extends Component {
 
   constructor(props) {
@@ -19,6 +53,20 @@ class App extends Component {
       currentPage: PAGES.MESSENGER,
       selectedEmojiIndex: 5,
     };
+
+    const protocol = getWsProtocol()
+
+    const ws = new ReconnectingWebsocket(`${protocol}${host}/socket`)
+    ws.onmessage = handleMessage.bind(this)
+
+    this.ws = ws
+
+  handleJoin(event) {
+    sendMessage(this.ws, {type: 'join', username: this.input.value})
+  }
+
+  handleStart(event) {
+    sendMessage(this.ws, {type: 'start'})
   }
 
   render() {
@@ -38,6 +86,13 @@ class App extends Component {
 
     return (
       <div className="App">
+        <div>
+          <h4>Debug actions</h4
+          <input ref={input => {this.input = input}} />
+          <button onClick={this.handleJoin.bind(this)}>join</button>
+          <button onClick={this.handleStart.bind(this)}>start</button>
+          <div>{this.emoji}</div>
+        </div>
         <p>
           Current Page: {currentPage}
         </p>
---
+++ server.py
@@ -0,0 +1,90 @@
+import json
+import os
+
+import gevent
+import redis
+from flask import Flask, render_template
+from flask_sockets import Sockets
+
+from random_emoji import random_emoji
+
+REDIS_URL = os.environ['REDIS_URL']
+REDIS_CHAN = 'emoji'
+
+app = Flask(__name__)
+app.debug = 'DEBUG' in os.environ
+
+sockets = Sockets(app)
+redis = redis.from_url(REDIS_URL)
+
+
+clients = {}
+
+pubsub = redis.pubsub()
+pubsub.subscribe(REDIS_CHAN)
+
+
+def send(client, data):
+    try:
+        client.send(data['data'])
+    except Exception:
+        app.logger.exception('Failed to send to client, removing from pool')
+        del clients[client]
+
+
+def send_message_to_all_clients():
+    for message in pubsub.listen():
+        for client in clients.keys():
+            gevent.spawn(send, client, message)
+
+
+gevent.spawn(send_message_to_all_clients)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def emoji_string(n):
+    return ''.join(random_emoji()[0] for _ in range(n))
+
+
+def handle_message(ws, data):
+    if data['type'] == 'join':
+        username = data['username']
+        clients[ws] = username
+
+        notify_all({
+            'type': 'join',
+            'username': username
+        })
+    elif data['type'] == 'start':
+        s = emoji_string(10)
+        print(s)
+        notify_all({
+            'type': 'start',
+            'emoji': s
+        })
+    else:
+        raise Exception('Unknown event {}'.format(data))
+
+
+def notify_all(data):
+    redis.publish(REDIS_CHAN, json.dumps(data).encode())
+
+
+@sockets.route('/socket')
+def handle_websocket(ws):
+    clients[ws] = {'username': None}
+
+    while not ws.closed:
+        message = ws.receive()
+        if message is None:
+            print('Got none message')
+        else:
+            data = json.loads(message)
+
+            handle_message(ws, data)
+
+        gevent.sleep(.1)
