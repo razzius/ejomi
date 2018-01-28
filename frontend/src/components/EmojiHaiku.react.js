8ea36756c763bb0afdcb8f22b761cf42cdefae0d
Razzi Abuissa <razzi53@gmail.com>
2018-01-28 13:25:20 -0800

    Add keys to emoji haiku

--- frontend/src/components/EmojiHaiku.react.js
+++ frontend/src/components/EmojiHaiku.react.js
@@ -16,8 +16,15 @@ class EmojiHaiku extends Component {
     var emojiWords = this.state.haiku.map(line => line.map(e => e.name).join(' '))
 
     return <div style={{paddingBottom: "25px"}}>
-      <h1>{emojiLines.map(line => <div>{line}</div>)}</h1>
-      <p style={{fontSize: "20px"}}>{emojiWords.map(line => <div><i>{line}</i></div>)}</p>
+      <h1>
+        {emojiLines.map((line, lineIndex) => <div key={lineIndex}>
+          {
+            line.map((e, index) => <span key={index} style={{padding: "10px"}}>{e}</span>)
+          }
+          </div>)
+        }
+      </h1>
+      <span style={{fontSize: "20px"}}>{emojiWords.map((line, index) => <p key={index}><i>{line}</i></p>)}</span>
     </div>
   }
 
