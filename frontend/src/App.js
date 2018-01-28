b925c5ec28cce293d9a4c00d819fc79658e8b345
Gregory Rose <grrosegr@users.noreply.github.com>
2018-01-27 16:26:30 -0800

    Fix merge issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReconnectingWebsocket from 'reconnecting-websocket'
+
 import './App.css';
 import Messenger from './Messenger.react';
+import Scrambler from './components/Scrambler.react';
 import Voter from './components/Voter.react';
+
+const PAGES = {
+  MESSENGER: 'MESSENGER',
+  SCRAMBLER: 'SCRAMBLER',
+  VOTER: 'VOTER',
+};
+
+const host = 'localhost:8000'
+
+function getWsProtocol() {
+  if (window.location.protocol === 'https:') {
+    return 'wss://'
+  } else {
+    return 'ws://'
+  }
+}
+
+function sendMessage(ws, message) {
+  ws.send(JSON.stringify(message))
+}
+
+function handleMessage(message) {
+  const reader = new FileReader()
+
+  reader.onload = () => {
+    const data = JSON.parse(reader.result)
+
+    console.log(`Received ${JSON.stringify(data)}`)
+
+    if (data.type === 'receive_emoji') {
+      var updatedState = this.state
+      updatedState['emoji'] = message.emoji
+      this.setState(updatedState)
+    }
+  }
+
+  reader.readAsText(message.data)
+}
 
 class App extends Component {
 
@@ -10,13 +50,27 @@ class App extends Component {
     super(props);
 
     this.state = {
+      currentPage: PAGES.MESSENGER,
       selectedEmojiIndex: 5,
     };
+
+    const protocol = getWsProtocol()
+
+    const ws = new ReconnectingWebsocket(`${protocol}${host}/socket`)
+    ws.onmessage = handleMessage.bind(this)
+
+    this.ws = ws
+  }
+
+  handleJoin(event) {
+    sendMessage(this.ws, {type: 'join', username: this.input.value})
+  }
+
+  handleStart(event) {
+    sendMessage(this.ws, {type: 'start'})
   }
 
   render() {
-<<<<<<< HEAD
-=======
     const {
       currentPage,
       selectedEmojiIndex,
@@ -31,11 +85,19 @@ class App extends Component {
       pageComponent = <Voter />;
     }
 
->>>>>>> 6072ccd492ea8c33cb3783b28e99c82b3acc1546
     return (
       <div className="App">
-        <Messenger selectedEmojiIndex={this.state.selectedEmojiIndex} lolwut={"hello"} />
-        <Voter />
+        <div>
+          <h4>Debug actions</h4>
+          <input ref={input => {this.input = input}} />
+          <button onClick={this.handleJoin.bind(this)}>join</button>
+          <button onClick={this.handleStart.bind(this)}>start</button>
+          <div>{this.emoji}</div>
+        </div>
+        <p>
+          Current Page: {currentPage}
+        </p>
+        {pageComponent}
       </div>
     );
   }
