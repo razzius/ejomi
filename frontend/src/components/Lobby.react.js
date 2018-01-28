8fa7d873836630b9b971261fca2a154ed579a1c1
shen <uashen@gmail.com>
2018-01-27 23:12:14 -0800

    heck yea font weight

--- frontend/src/components/Lobby.react.js
+++ frontend/src/components/Lobby.react.js
@@ -8,20 +8,28 @@ class Lobby extends Component {
     this.colorHash = new ColorHash();
     this.state = {
       showJoin: true,
+      currentUser: ""
     }
   }
 
   _renderName = (name) => {
     const colorHex = this.colorHash.hex(name);
-    const style = { color: colorHex }
+    const isCurrentUser = name === this.state.currentUser;
+    const fontWeight = isCurrentUser ? "bold" : "normal"
+    const style = { color: colorHex,
+                    "font-weight": fontWeight}
     return <p className="userInList" key={name} style={style}>{name}</p>;
   }
 
   _onJoin = () => {
-    this.props.onJoin(this.input.value)
+    if (this.input.value.length == 0) {
+      return;
+    }
     this.setState({
-      showJoin: false
+      showJoin: false,
+      currentUser: this.input.value
     });
+    this.props.onJoin(this.input.value)
   }
 
   render() {
@@ -45,6 +53,7 @@ class Lobby extends Component {
       <div>
         {inputForm}
         {startButton}
+        <h5>Users:</h5>
         {this.props.userList.map(this._renderName)}
       </div>
     );
