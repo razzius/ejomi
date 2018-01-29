8bd4b8840e26a7ca362ebce6a476a9faf0c4a810
Claire Tang <clairetang6@gmail.com>
2018-01-28 17:33:48 -0800

    move revealing message specific code out of Revealer

--- frontend/src/components/RevealingMessage.react.js
+++ frontend/src/components/RevealingMessage.react.js
@@ -4,13 +4,37 @@ import './RevealingMessage.css'
 
 class RevealingMessage extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      transitioningTo: 'original',
+    }
+
+    this.textToDisplay = {
+      'original': "Original message:",
+      'scrambled': "Scrambled message:",
+    }
+  }
+
   render() {
+    this.letterPairs = [];
+    for(let i = 0; i < this.props.originalMessage.length; i++){
+      let originalLetter = this.props.originalMessage.charAt(i);
+      let scrambledLetter = this.props.scrambledMessage.charAt(i);
+      this.letterPairs.push({'originalLetter': originalLetter, 'scrambledLetter': scrambledLetter});
+    }
 
     return (
+        <div>
+      <p>
+         {this.textToDisplay[this.state.transitioningTo]}
+      </p>
       <div className='revealingMessage'>
-          {this.props.letterPairs.map(function(letters, index){
-            return <RevealingLetter originalLetter={letters.originalLetter} scrambledLetter={letters.scrambledLetter}/>;
-          })}
+        {this.letterPairs.map(function(letters, index){
+          return <RevealingLetter originalLetter={letters.originalLetter} scrambledLetter={letters.scrambledLetter}/>;
+        })}
+      </div>
       </div>
     );
   }
